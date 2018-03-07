e037e89eba18fd65ef25be14f4bdb9afddd00cee
Markham Shofner <markham.shofner@gmail.com>
2018-03-06 21:44:18 -0800

    clean up and draft working version of file io

--- data-structures/Lab-1/PostfixExpressor/src/postfixexpressor/PostfixExpressor.java
+++ data-structures/Lab-1/PostfixExpressor/src/postfixexpressor/PostfixExpressor.java
@@ -9,16 +9,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
- 
-// operators/operands on off the stack in the right order
-
-// TD - temp variables for data 
-
-// map the operators to text language
-
-
-// producing the machine language (strings) around that
-
 /**
  *
  * @author mark8604
@@ -29,46 +19,36 @@ public class PostfixExpressor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // set up the stack
-//        PostfixStack myPostfixStack = new PostfixStack();
-        
-        translatePostfix("AB+C-");
-        
-        return;
-
-        // read the data
+        // set up file read and write
         try {
             FileReader fr = new FileReader("../ProvidedStrings.txt"); // input file
-            FileWriter fw = new FileWriter("../OutputStrings.txt"); // output file
-
             int c;
-            while ((c = fr.read()) != -1) { // read and process one character
-                if (isOperator(c)) { // replace all occurrences of 'a' with '@'
-                    myPostfixStack.push(c);
-                    System.out.println(myPostfixStack.head);
-                    System.out.println('0');
-                    fw.write('t');
-                }
-                else { 
-                    fw.write(c);
-                    myPostfixStack.push(c);
-                    System.out.println(myPostfixStack.head.data);
-                    char testChar;
-                    try {
-                        testChar = intValue(c);
-                    } catch (Exception e) {
-                        testChar = 0;
-                        System.out.println("Error: " + e);
+            int fileCount = 1;
+            while((c = fr.read()) !=-1) {
+                String inputString = "";
+                try {
+                    while ((c = fr.read()) != 10) {
+                        System.out.println(c);
+                        char cChar = intValue(c);
+                        inputString = inputString+cChar;
+                        System.out.println(inputString);
                     }
-                    System.out.println(testChar);
-//                    System.out.println('1');
+                } catch (Exception e) {
+                    System.out.println("Error: " + e); 
                 }
+                String translatedString = translatePostfix(inputString);
+                System.out.println("--------------------");
+                System.out.println(translatedString);
+
+                // open, transfer, and close file writer
+                FileWriter fw = new FileWriter("../OutputStrings" + fileCount + ".txt"); // output file
+                fw.write(translatedString);
+                fw.close();
+                fileCount++;
             }
-            System.out.println(myPostfixStack);
-            fw.close();
         } catch (IOException e) {
             System.out.println("I/O Error: " + e);
-        } 
+        }
     }
         
     public static char intValue (int i) throws Exception {
@@ -91,21 +71,12 @@ public class PostfixExpressor {
     
     public static String translatePostfix (String pString) {
         String translatedString = "";
-        PostfixStack myPostfixStack = new PostfixStack();
-
-//        pString = "ABC+*CBA-+*";
-//        pString = "ABC+*/";
-//        pString ="ABCD-+*EF*-";
-        pString ="ABC*+DE-/";
-
-        System.out.println(pString);
-  
+        PostfixStack myPostfixStack = new PostfixStack();  
         int varMule = 100;
         for (int j=0; j<pString.length(); j++) {
             int i = pString.charAt(j);
             // if it's an operator, pop the stack twice and perform an operation 
             // with those values and push the result back onto the stack
-            
             if (isOperator(i)) { 
                 char iChar = (char) i;
                 char c = (char) myPostfixStack.pop();
@@ -114,14 +85,13 @@ public class PostfixExpressor {
                 int t = varMule;
                 int varCount = t-99;
                 varMule++;
-                
-                String varString = "TEMP"+varCount;
-
                 myPostfixStack.push(t);
-                
+
                 String cString;
                 String dString;
-                
+                String varString = "TEMP"+varCount;
+                String convertedOperator = translateOperator(iChar);
+
                 if (c>99) {
                     int cCount = c-99;
                     cString = "TEMP"+cCount;
@@ -135,18 +105,15 @@ public class PostfixExpressor {
                     dString = "" + d;
                 }
                 
-                String convertedOperator = translateOperator(iChar);
                 
-                System.out.println("LD " + "\t"+ dString);
-                System.out.println(convertedOperator + "\t"+ cString);
-                System.out.println("ST " + "\t" + varString);                
+                translatedString = translatedString + "LD " + "\t"+ dString + "\n";
+                translatedString = translatedString + convertedOperator + "\t"+ cString + "\n";
+                translatedString = translatedString + "ST " + "\t" + varString + "\n";
             } else { // if it's an operand, push it onto the stack
                 myPostfixStack.push(i);
             }
-
         }
-        System.out.println("Translated String: " + translatedString);
-        return translatedString; // 
+        return translatedString;
     }
     
     // translateOperatior
@@ -160,7 +127,7 @@ public class PostfixExpressor {
             case 43:
                 return "AD";
             case 45:
-                return "ST";
+                return "SB";
             case 47:
                 return "DV";
         }
@@ -169,15 +136,5 @@ public class PostfixExpressor {
     public static boolean isOperator (int i) {
         // +, -, *, /
         return i==42 || i==43 || i==45 || i==47; 
-    }
-    
-    public static int operatorPrecedence (int i) {
-        int operatorPrecedence = 0;
-        if (i==43 || i==45) { // +,- have a lower precedence
-            operatorPrecedence = 1;
-        } else if (i==42 || i==47) { // *,/ have a higher precedence
-            operatorPrecedence = 2;
-        }
-        return operatorPrecedence;
     }
 }
