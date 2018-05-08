caa2f41f1214b2150f7349535d71dbef2a1fc449
Markham Shofner <markham.shofner@gmail.com>
2018-05-07 22:46:08 -0400

    account for new lines and carriage returns

--- data-structures/Lab-4/SortComparison/src/sortcomparison/SortComparison.java
+++ data-structures/Lab-4/SortComparison/src/sortcomparison/SortComparison.java
@@ -29,34 +29,24 @@ public class SortComparison {
         try(BufferedReader br = new BufferedReader(new FileReader("../InputData/ran50.dat"))) {
             System.out.println("---Begin File Read---");
             int c;
-
             int r;
             int i = 0;
             String numberString = "";
 
-            // make sure to grab last value
-            // properly handle white spaces
-            // put into the array
-
-            // read char
-                // if space
-                    // if there is value, send string into array as int
-                    // if empty space, continue
-                // else
-                    // add to existing numberString
-
-
             while ((r = br.read()) != -1) {
                 char ch = (char) r;
-
-                if ((r) == 32) {
+                // if a space, new line, or carriage return
+                if (((r) == 32) || ((r) == 10) || ((r) == 13)) {
                     if (numberString.trim().length() > 0) {
+                        System.out.println("--" + numberString + "--");
+                        
                         dataArray[i] = Integer.parseInt(numberString);
                         i++;
-                        numberString = "";
                     }
+                    numberString = "";
                 } else {
                     numberString = numberString + ch;
+                    System.out.println("Add " + ch + " to " + numberString);
                 }
             }
         }
@@ -64,68 +54,17 @@ public class SortComparison {
             System.out.println("Error: " + e);
         }
 
-
         int arr[] = {3, 5, 7, 1, 2, 99, 12, -1, 45, 69, 100000};
 //        int length = arr.length;
         int length = dataArray.length;
 //        heapSort(dataArray, length);
 //       quickSort(arr, 0, arr.length-1);
-         quickSort(dataArray, 0, dataArray.length-1);
+        quickSort(dataArray, 0, dataArray.length-1);
+//        for (int i=0; i<dataArray.length; ++i) {
+//            System.out.println(dataArray[i]);
+//        }
     }
 
-    // Build the heap and then extract the elements in sorted order from the heap.
-    public static void heapSort (int arr[], int length) {
-        for (int i=0; i<length; ++i) {
-            System.out.println(arr[i]);
-        }
-
-        // Heapify the array
-        for (int i = length / 2 - 1; i >= 0; i--) {
-            MaxHeapPercolateDown(i, arr, length);
-        }
-        // Remove the maximum value, store that value at the end index (since that is where the highest val should go in an ordered array)
-        // then Decrement the end index, until the end index is 0.
-        for (int i = length - 1; i > 0; i--) {
-            int tempInt = arr[0];
-            arr[0]=arr[i];
-            arr[i]=tempInt;
-            MaxHeapPercolateDown(0, arr, i);
-        }
-        System.out.println("---------------------------");
-        for (int i=0; i<length; ++i) {
-            System.out.println(arr[i]);
-        }
-        //        return sortedArr;
-    }
-
-    public static int MaxHeapPercolateDown(int nodeIndex, int heapArray[], int arraySize) {
-        int childIndex = 2 * nodeIndex + 1;
-        int value = heapArray[nodeIndex];
-
-        while (childIndex < arraySize) {
-            // Find the max among the node and all the node's children
-            int maxValue = value;
-            int maxIndex = -1;
-            for (int i = 0; i < 2 && i + childIndex < arraySize; i++) {
-                if (heapArray[i + childIndex] > maxValue) {
-                    maxValue = heapArray[i + childIndex];
-                    maxIndex = i + childIndex;
-                }
-            }
-
-            if (maxValue == value) {
-                  return value;
-            }
-            else {
-                int tempInt = heapArray[nodeIndex];
-                heapArray[nodeIndex] = heapArray[maxIndex];
-                heapArray[maxIndex] = tempInt;
-                nodeIndex = maxIndex;
-                childIndex = 2 * nodeIndex + 1;
-            }
-        }
-        return -1;
-    }
 
     // pull from zybook for quicksort roots
 
@@ -208,5 +147,58 @@ public class SortComparison {
 
     // insertion sort
         // linked implementation better?
+    
+    // Build the heap and then extract the elements in sorted order from the heap.
+    public static void heapSort (int arr[], int length) {
+        for (int i=0; i<length; ++i) {
+            System.out.println(arr[i]);
+        }
 
+        // Heapify the array
+        for (int i = length / 2 - 1; i >= 0; i--) {
+            MaxHeapPercolateDown(i, arr, length);
+        }
+        // Remove the maximum value, store that value at the end index (since that is where the highest val should go in an ordered array)
+        // then Decrement the end index, until the end index is 0.
+        for (int i = length - 1; i > 0; i--) {
+            int tempInt = arr[0];
+            arr[0]=arr[i];
+            arr[i]=tempInt;
+            MaxHeapPercolateDown(0, arr, i);
+        }
+        System.out.println("---------------------------");
+        for (int i=0; i<length; ++i) {
+            System.out.println(arr[i]);
+        }
+        //        return sortedArr;
+    }
+
+    public static int MaxHeapPercolateDown(int nodeIndex, int heapArray[], int arraySize) {
+        int childIndex = 2 * nodeIndex + 1;
+        int value = heapArray[nodeIndex];
+
+        while (childIndex < arraySize) {
+            // Find the max among the node and all the node's children
+            int maxValue = value;
+            int maxIndex = -1;
+            for (int i = 0; i < 2 && i + childIndex < arraySize; i++) {
+                if (heapArray[i + childIndex] > maxValue) {
+                    maxValue = heapArray[i + childIndex];
+                    maxIndex = i + childIndex;
+                }
+            }
+
+            if (maxValue == value) {
+                  return value;
+            }
+            else {
+                int tempInt = heapArray[nodeIndex];
+                heapArray[nodeIndex] = heapArray[maxIndex];
+                heapArray[maxIndex] = tempInt;
+                nodeIndex = maxIndex;
+                childIndex = 2 * nodeIndex + 1;
+            }
+        }
+        return -1;
+    }
 }
