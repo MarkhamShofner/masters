c3f553fa4437a9c004d0f044318663d8751df8a4
Markham Shofner <markham.shofner@gmail.com>
2018-07-09 15:36:23 -0700

    clean up results and fix off by one loop issue

--- computer-organizaiton/HW-6-Lottery/Jackpot-Probabilities.asm
+++ computer-organizaiton/HW-6-Lottery/Jackpot-Probabilities.asm
@@ -6,12 +6,13 @@
 # Let's invert this formula to be (pool!)/[(pick!)*(pool-pick)!]
 # Since the largest MIPS single precision integer value will not hold the value of more than 12 Factorial
 # we need to adjust this formula to reflect that (pool!)/(pick!) = (pool)*(pool-1)*(pool-2)*...(pool-pick)
-# Leading us to (modifiedPool)!/(pick)!
+# Leading us to (modifiedPool)!/(pick)! for each group. Then groupA*groupB for combined chance.
 
 .globl main
 main:   # winning lottery odds based on passed in parameters
         .text
 
+        # grab the params
         li          $v0, 4        # system call code for print_str
         la          $a0, strLrg1  # address of string to print
         syscall                   # print the string
@@ -44,64 +45,93 @@ main:   # winning lottery odds based on passed in parameters
         syscall                   # get int
         move        $s4, $v0      # save Small Pick in $s4
 
-
-        # (pool!)/[(pick!)*(pool-pick)!]
-        # pool!
-        move        $a0, $s1      # Large Pool to arg0
-        jal         factrl        # run the factorial method
-        move        $t7, $v0      # store the result
+        li          $v0, 4        # system call code for print_str
+        la          $a0, str      # address of string to print
+        syscall                   # print the string
 
         # modifiedPool! (Large)
         move        $a0, $s1      # Large Pool to arg0
         sub         $a1, $s1, $s2 # (Large Pool - Large Pick) to find stopping point
         add         $a1, $a1, 2   # add 2 to the argument to avoid extra loops
         jal         modfactrl     # run the factorial method
+        move        $t7, $v0      # store result as temp
+        slt         $t6, $t7, 2   # is the result less than 2 (was the modfactorial return off)
+        beq         $t6, $zero, ElseLrg # if not jump and store
+        move        $t2, $s1      # if true store as pool
+        j           EndLrg        # end
+  ElseLrg:
         move        $t2, $v0      # store the result
+  EndLrg:
 
         # pick! (Large)
         move        $a0, $s2      # Large Pick to arg0
         jal         factrl        # run the factorial method
         move        $t3, $v0      # store the result
 
-        # Cdds of getting Large Pool correct
+        # Odds of getting Large Pool correct
         div         $s5, $t2, $t3
 
-        # (pool-pick)!
-        # sub         $t0, $s1, $s2 # calculate pool-pick
-        # li          $v0, 1        # system call code for print_int
-        # move        $a0, $t0      # integer to print // $s1
-        # syscall                   # print it
+        # modifiedPool! (Small)
+        move        $a0, $s3      # Small Pool to arg0
+        sub         $a1, $s3, $s4 # (Small Pool - Small Pick) to find stopping point
+        add         $a1, $a1, 2   # add 2 to the argument to avoid extra loops
+        jal         modfactrl     # run the factorial method
+        move        $t7, $v0      # store result as temp
+        slt         $t6, $t7, 2   # is the result less than 2 (was the modfactorial return off)
+        beq         $t6, $zero, ElseSml # if not jump and store
+        move        $t4, $s3      # if true store as pool
+        j           EndSm         # end
+  ElseSml:
+        move        $t4, $v0      # store the result
+  EndSm:
 
+        # pick! (Small)
+        move        $a0, $s4      # Small Pick to arg0
+        jal         factrl        # run the factorial method
+        move        $t5, $v0      # store the result
 
-        #  move        $a0, $s1      # Large Pool to arg0
-        #  move        $a1, $s2      # Large Pick to arg1
-        #  jal         factrl        # run the factorial method
-        #  move        $t7, $v0      # store the result
+        # Odds of getting Small Pool correct
+        div         $s6, $t4, $t5
+
+        # Odds of getting both pools correct
+        mul         $s0, $s5, $s6
+
+        # Print result
+        li          $v0, 1        # system call code for print_int
+        move        $a0, $s0      # integer to print
+        syscall                   # print it
 
         # For debugging
-        li          $v0, 1        # system call code for print_int
-        move        $a0, $t7      # integer to print // $s1
-        syscall                   # print it
-        li          $v0, 1        # system call code for print_int
-        move        $a0, $zero    # integer to print // $s1
-        syscall                   # print it
-        li          $v0, 1        # system call code for print_int
-        move        $a0, $t2      # integer to print // $s1
-        syscall                   # print it
-        li          $v0, 1        # system call code for print_int
-        move        $a0, $zero    # integer to print // $s1
-        syscall                   # print it
-        li          $v0, 1        # system call code for print_int
-        move        $a0, $t3      # integer to print // $s1
-        syscall                   # print it
-        li          $v0, 1        # system call code for print_int
-        move        $a0, $zero    # integer to print // $s1
-        syscall                   # print it
-        li          $v0, 1        # system call code for print_int
-        move        $a0, $s5     # integer to print // $s1
-        syscall                   # print it
-        li          $v0, 1        # system call code for print_int
-        move        $a0, $zero    # integer to print // $s1
+        # li          $v0, 1        # system call code for print_int
+        # move        $a0, $zero    # integer to print // $s1
+        # syscall                   # print it
+        # li          $v0, 1        # system call code for print_int
+        # move        $a0, $t2      # integer to print // $s1
+        # syscall                   # print it
+        # li          $v0, 1        # system call code for print_int
+        # move        $a0, $zero    # integer to print // $s1
+        # syscall                   # print it
+        # li          $v0, 1        # system call code for print_int
+        # move        $a0, $t3      # integer to print // $s1
+        # syscall                   # print it
+        # li          $v0, 1        # system call code for print_int
+        # move        $a0, $zero    # integer to print // $s1
+        # syscall                   # print it
+        # li          $v0, 1        # system call code for print_int
+        # move        $a0, $s5      # integer to print // $s1
+        # syscall                   # print it
+        # li          $v0, 1        # system call code for print_int
+        # move        $a0, $zero    # integer to print // $s1
+        # syscall
+        # li          $v0, 1        # system call code for print_int
+        # move        $a0, $s6      # integer to print // $s1
+        # syscall                   # print it
+        # li          $v0, 1        # system call code for print_int
+        # move        $a0, $zero    # integer to print // $s1
+        # syscall
+        # li          $v0, 1        # system call code for print_int
+        # move        $a0, $s0      # integer to print // $s1
+        # syscall                   # print it
 
 
         # End Program
@@ -137,13 +167,13 @@ L1:     addi $a0, $a0, -1           # n = n-1
 # Given n and r, in registers $a0 and $s1;
 # calculate (n)*(n-1)*(n-2)*...(r), store and return the result in register $v0
 
-modfactrl: sw $ra, 4($sp)           # save the return address
+modfactrl: sw $ra, 4($sp)           # save the return address mod
         sw $a0, 0($sp)              # save the current value of n
         addi $sp, $sp, -8           # move stack pointer
         # slti $t0, $a0, 2          # save 1 iteration, n=0 or n=1; n!=1
         # beq $t0, $a1, Loop1       # not less than $a1, calculate n(n-1)!
         slt $t0, $a0, $a1           # save 1 iteration, n=0 or n=1; n!=1
-        beq $t0, $zero, Loop1       # not less than 2, calculate n(n-1)!
+        beq $t0, $zero, Loop1       # not less than $a0, calculate n(n-1)!
         addi $v0, $zero, 1          # n=1; n!=1
         jr $ra                      # now multiply
 
@@ -158,8 +188,6 @@ Loop1:  addi $a0, $a0, -1           # n = n-1
 # P Snyder 14 August 2016, modified by M Shofner 9 July 2018
 ######### End of the subroutine
 
-
-
         .data
 str:
             .asciiz "The odds are 1 in "
@@ -171,5 +199,3 @@ strSml1:
             .asciiz "Size of the small pool? \n"
 strSml2:
             .asciiz "Picks needed from the small pool? \n"
-stopped:
-            .asciiz "\nStopped."
