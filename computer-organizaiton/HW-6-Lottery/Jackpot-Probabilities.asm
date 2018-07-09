58e5f8dadc16b2ab9ee4d289cfc5efd73875c75e
Markham Shofner <markham.shofner@gmail.com>
2018-07-09 11:18:44 -0700

    modified factorial subroutine and simplification logic

--- computer-organizaiton/HW-6-Lottery/Jackpot-Probabilities.asm
+++ computer-organizaiton/HW-6-Lottery/Jackpot-Probabilities.asm
@@ -4,6 +4,9 @@
 # Which can be simplified to [(pick!)*(pool-pick)!]/(pool!)
 # Since the output requested for this exercise is not the chance of the occurrence but rather the 1 in X form,
 # Let's invert this formula to be (pool!)/[(pick!)*(pool-pick)!]
+# Since the largest MIPS single precision integer value will not hold the value of more than 12 Factorial
+# we need to adjust this formula to reflect that (pool!)/(pick!) = (pool)*(pool-1)*(pool-2)*...(pool-pick)
+# Leading us to (modifiedPool)!/(pick)!
 
 .globl main
 main:   # winning lottery odds based on passed in parameters
@@ -44,13 +47,25 @@ main:   # winning lottery odds based on passed in parameters
 
         # (pool!)/[(pick!)*(pool-pick)!]
         # pool!
-        # move        $a0, $s1      # Large Pool to arg0
-        # jal         factrl        # run the factorial subroutine
-        # move        $s5, $v0      # store the result
-        # # pick!
-        # move        $a0, $s2      # Large Pick to arg0
-        # jal         factrl        # run the factorial subroutine
-        # move        $s6, $v0      # store the result
+        move        $a0, $s1      # Large Pool to arg0
+        jal         factrl        # run the factorial method
+        move        $t7, $v0      # store the result
+
+        # modifiedPool! (Large)
+        move        $a0, $s1      # Large Pool to arg0
+        sub         $a1, $s1, $s2 # (Large Pool - Large Pick) to find stopping point
+        add         $a1, $a1, 2   # add 2 to the argument to avoid extra loops
+        jal         modfactrl     # run the factorial method
+        move        $t2, $v0      # store the result
+
+        # pick! (Large)
+        move        $a0, $s2      # Large Pick to arg0
+        jal         factrl        # run the factorial method
+        move        $t3, $v0      # store the result
+
+        # Cdds of getting Large Pool correct
+        div         $s5, $t2, $t3
+
         # (pool-pick)!
         # sub         $t0, $s1, $s2 # calculate pool-pick
         # li          $v0, 1        # system call code for print_int
@@ -58,31 +73,92 @@ main:   # winning lottery odds based on passed in parameters
         # syscall                   # print it
 
 
-
-
-      #  move        $a0, $s1      # Large Pool to arg0
-      #  move        $a1, $s2      # Large Pick to arg1
-      #  jal         factrl        # run the factorial subroutine
-      #  move        $s5, $v0      # store the result
+        #  move        $a0, $s1      # Large Pool to arg0
+        #  move        $a1, $s2      # Large Pick to arg1
+        #  jal         factrl        # run the factorial method
+        #  move        $t7, $v0      # store the result
 
         # For debugging
         li          $v0, 1        # system call code for print_int
-        move        $a0, $s1      # integer to print // $s1
+        move        $a0, $t7      # integer to print // $s1
         syscall                   # print it
         li          $v0, 1        # system call code for print_int
-        move        $a0, $s2      # integer to print // $s1
+        move        $a0, $zero    # integer to print // $s1
         syscall                   # print it
         li          $v0, 1        # system call code for print_int
-        move        $a0, $s3      # integer to print // $s1
+        move        $a0, $t2      # integer to print // $s1
         syscall                   # print it
         li          $v0, 1        # system call code for print_int
-        move        $a0, $s4      # integer to print // $s1
+        move        $a0, $zero    # integer to print // $s1
         syscall                   # print it
+        li          $v0, 1        # system call code for print_int
+        move        $a0, $t3      # integer to print // $s1
+        syscall                   # print it
+        li          $v0, 1        # system call code for print_int
+        move        $a0, $zero    # integer to print // $s1
+        syscall                   # print it
+        li          $v0, 1        # system call code for print_int
+        move        $a0, $s5     # integer to print // $s1
+        syscall                   # print it
+        li          $v0, 1        # system call code for print_int
+        move        $a0, $zero    # integer to print // $s1
 
 
         # End Program
         li $v0, 10
         syscall
+
+######### Factorial Subroutine Fall 2016
+#
+# Given n, in register $a0;
+# calculate n!, store and return the result in register $v0
+
+factrl: sw $ra, 4($sp)              # save the return address
+        sw $a0, 0($sp)              # save the current value of n
+        addi $sp, $sp, -8           # move stack pointer
+        slti $t0, $a0, 2            # save 1 iteration, n=0 or n=1; n!=1
+        beq $t0, $zero, L1          # not less than 2, calculate n(n-1)!
+        addi $v0, $zero, 1          # n=1; n!=1
+        jr $ra                      # now multiply
+
+L1:     addi $a0, $a0, -1           # n = n-1
+        jal factrl                  # now (n-1)!
+        addi $sp, $sp, 8            # reset the stack pointer
+        lw $a0, 0($sp)              # fetch saved (n-1)
+        lw $ra, 4($sp)              # fetch return address
+        mul $v0, $a0, $v0           # multiply (n)*(n-1)
+        jr $ra                      # return value n!
+
+# P Snyder 14 August 2016
+######### End of the subroutine
+
+######### Modified Factorial Subroutine Summer 2018
+#
+# Given n and r, in registers $a0 and $s1;
+# calculate (n)*(n-1)*(n-2)*...(r), store and return the result in register $v0
+
+modfactrl: sw $ra, 4($sp)           # save the return address
+        sw $a0, 0($sp)              # save the current value of n
+        addi $sp, $sp, -8           # move stack pointer
+        # slti $t0, $a0, 2          # save 1 iteration, n=0 or n=1; n!=1
+        # beq $t0, $a1, Loop1       # not less than $a1, calculate n(n-1)!
+        slt $t0, $a0, $a1           # save 1 iteration, n=0 or n=1; n!=1
+        beq $t0, $zero, Loop1       # not less than 2, calculate n(n-1)!
+        addi $v0, $zero, 1          # n=1; n!=1
+        jr $ra                      # now multiply
+
+Loop1:  addi $a0, $a0, -1           # n = n-1
+        jal modfactrl               # now (n-1)!
+        addi $sp, $sp, 8            # reset the stack pointer
+        lw $a0, 0($sp)              # fetch saved (n-1)
+        lw $ra, 4($sp)              # fetch return address
+        mul $v0, $a0, $v0           # multiply (n)*(n-1)
+        jr $ra                      # return value n!
+
+# P Snyder 14 August 2016, modified by M Shofner 9 July 2018
+######### End of the subroutine
+
+
 
         .data
 str:
@@ -97,24 +173,3 @@ strSml2:
             .asciiz "Picks needed from the small pool? \n"
 stopped:
             .asciiz "\nStopped."
-
-######### Factorial Subroutine Fall 2016
-#
-# Given n, in register $a0;
-# calculate n!, store and return the result in register $v0
-factrl: sw $ra, 4($sp) # save the return address
-        sw $a0, 0($sp) # save the current value of n
-        addi $sp, $sp, -8 # move stack pointer
-        slti $t0, $a0, 2 # save 1 iteration, n=0 or n=1; n!=1
-        beq $t0, $zero, L1 # not less than 2, calculate n(n-1)!
-        addi $v0, $zero, 1 # n=1; n!=1
-        jr $ra # now multiply
-L1:     addi $a0, $a0, -1 # n = n-1
-        jal factrl # now (n-1)!
-        addi $sp, $sp, 8 # reset the stack pointer
-        lw $a0, 0($sp) # fetch saved (n-1)
-        lw $ra, 4($sp) # fetch return address
-        mul $v0, $a0, $v0 # multiply (n)*(n-1)
-        jr $ra # return value n!
-# P Snyder 14 August 2016
-######### End of the subroutine
