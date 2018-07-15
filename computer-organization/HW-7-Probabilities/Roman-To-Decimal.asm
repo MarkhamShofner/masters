647ab76613294993141c278424fc3a65b8614dc8
Markham Shofner <markham.shofner@gmail.com>
2018-07-15 19:07:31 -0400

    working crosswalk of strings/chars and initial value add

--- computer-organization/HW-7-Probabilities/Roman-To-Decimal.asm
+++ computer-organization/HW-7-Probabilities/Roman-To-Decimal.asm
@@ -1,201 +1,148 @@
-# Applying the following logic. If there are 50 numbers in a pool and we need to pick 5 to win.
-# The chance we pick _one_ of the numbers is 5 in 50. Then the chance we pick a _second_ number is 4 in 49.
-# Then 3 in 48, etc... down to 1 in 46. So (5/50*4/49*3/48*2/47*1/46). This would be (pick!)/[(pool!)/(pool-pick)!]
-# Which can be simplified to [(pick!)*(pool-pick)!]/(pool!)
-# Since the output requested for this exercise is not the chance of the occurrence but rather the 1 in X form,
-# Let's invert this formula to be (pool!)/[(pick!)*(pool-pick)!]
-# Since the largest MIPS single precision integer value will not hold the value of more than 12 Factorial
-# we need to adjust this formula to reflect that (pool!)/(pick!) = (pool)*(pool-1)*(pool-2)*...(pool-pick)
-# Leading us to (modifiedPool)!/(pick)! for each group. Then groupA*groupB for combined chance.
 
-.globl main
-main:   # winning lottery odds based on passed in parameters
-        .text
+            .globl main
+main:       # accent Roman numeral input and output its decimal value
+            .text
 
-        # grab the params
-        li          $v0, 4        # system call code for print_str
-        la          $a0, strLrg1  # address of string to print
-        syscall                   # print the string
+            # grab the Roman string
+            li          $v0, 4        # system call code for print_str
+            la          $a0, str      # address of string to print
+            syscall                   # print the string
 
-        li          $v0, 5        # system call code for prompt_int
-        syscall                   # get int
-        move        $s1, $v0      # save Large Pool in $s1
+            li          $v0, 8        # system call code for read_string
+            la          $a0, buffer   # load byte space into arg0
+            li          $a1, 16       # assign byte space allocation in arg1
+            syscall                   # read the string
+            la          $s1, buffer   # save the string in $s1
 
-        li          $v0, 4        # system call code for print_str
-        la          $a0, strLrg2  # address of string to print
-        syscall                   # print the string
+            li          $v0, 4        # system call code for print_str
+            move        $a0, $s1      # address of string to print
+            syscall                   # print the string
 
-        li          $v0, 5        # system call code for prompt_int
-        syscall                   # get int
-        move        $s2, $v0      # save Large Pick in $s2
+            la          $t3, romanI   # load I into a temp variable
+            la          $t4, romanV   # load V into a temp variable
+            la          $t5, romanX   # load X into a temp variable
+            la          $t6, romanL   # load L into a temp variable
+            la          $t7, romanC   # load C into a temp variable
+            la          $t8, romanD   # load D into a temp variable
+            la          $t9, romanM   # load M into a temp variable
 
-        li          $v0, 4        # system call code for print_str
-        la          $a0, strSml1  # address of string to print
-        syscall                   # print the string
+            # debugging
+            # li          $v0, 4        # system call code for print_str
+            # move        $a0, $t3      # address of string to print
+            # syscall                   # print the string
 
-        li          $v0, 5        # system call code for prompt_int
-        syscall                   # get int
-        move        $s3, $v0      # save Small Pool in $s3
+            # li          $v0, 4        # system call code for print_str
+            # move        $a0, $t4      # address of string to print
+            # syscall                   # print the string
 
-        li          $v0, 4        # system call code for print_str
-        la          $a0, strSml2  # address of string to print
-        syscall                   # print the string
+            # li          $v0, 4        # system call code for print_str
+            # move        $a0, $t8      # address of string to print
+            # syscall                   # print the string
 
-        li          $v0, 5        # system call code for prompt_int
-        syscall                   # get int
-        move        $s4, $v0      # save Small Pick in $s4
-
-        li          $v0, 4        # system call code for print_str
-        la          $a0, str      # address of string to print
-        syscall                   # print the string
-
-        # modifiedPool! (Large)
-        move        $a0, $s1      # Large Pool to arg0
-        sub         $a1, $s1, $s2 # (Large Pool - Large Pick) to find stopping point
-        add         $a1, $a1, 2   # add 2 to the argument to avoid extra loops
-        jal         modfactrl     # run the factorial method
-        move        $t7, $v0      # store result as temp
-        slt         $t6, $t7, 2   # is the result less than 2 (was the modfactorial return off)
-        beq         $t6, $zero, ElseLrg # if not jump and store
-        move        $t2, $s1      # if true store as pool
-        j           EndLrg        # end
-  ElseLrg:
-        move        $t2, $v0      # store the result
-  EndLrg:
-
-        # pick! (Large)
-        move        $a0, $s2      # Large Pick to arg0
-        jal         factrl        # run the factorial method
-        move        $t3, $v0      # store the result
-
-        # Odds of getting Large Pool correct
-        div         $s5, $t2, $t3
-
-        # modifiedPool! (Small)
-        move        $a0, $s3      # Small Pool to arg0
-        sub         $a1, $s3, $s4 # (Small Pool - Small Pick) to find stopping point
-        add         $a1, $a1, 2   # add 2 to the argument to avoid extra loops
-        jal         modfactrl     # run the factorial method
-        move        $t7, $v0      # store result as temp
-        slt         $t6, $t7, 2   # is the result less than 2 (was the modfactorial return off)
-        beq         $t6, $zero, ElseSml # if not jump and store
-        move        $t4, $s3      # if true store as pool
-        j           EndSm         # end
-  ElseSml:
-        move        $t4, $v0      # store the result
-  EndSm:
-
-        # pick! (Small)
-        move        $a0, $s4      # Small Pick to arg0
-        jal         factrl        # run the factorial method
-        move        $t5, $v0      # store the result
-
-        # Odds of getting Small Pool correct
-        div         $s6, $t4, $t5
-
-        # Odds of getting both pools correct
-        mul         $s0, $s5, $s6
-
-        # Print result
-        li          $v0, 1        # system call code for print_int
-        move        $a0, $s0      # integer to print
-        syscall                   # print it
-
-        # For debugging
-        # li          $v0, 1        # system call code for print_int
-        # move        $a0, $zero    # integer to print // $s1
-        # syscall                   # print it
-        # li          $v0, 1        # system call code for print_int
-        # move        $a0, $t2      # integer to print // $s1
-        # syscall                   # print it
-        # li          $v0, 1        # system call code for print_int
-        # move        $a0, $zero    # integer to print // $s1
-        # syscall                   # print it
-        # li          $v0, 1        # system call code for print_int
-        # move        $a0, $t3      # integer to print // $s1
-        # syscall                   # print it
-        # li          $v0, 1        # system call code for print_int
-        # move        $a0, $zero    # integer to print // $s1
-        # syscall                   # print it
-        # li          $v0, 1        # system call code for print_int
-        # move        $a0, $s5      # integer to print // $s1
-        # syscall                   # print it
-        # li          $v0, 1        # system call code for print_int
-        # move        $a0, $zero    # integer to print // $s1
-        # syscall
-        # li          $v0, 1        # system call code for print_int
-        # move        $a0, $s6      # integer to print // $s1
-        # syscall                   # print it
-        # li          $v0, 1        # system call code for print_int
-        # move        $a0, $zero    # integer to print // $s1
-        # syscall
-        # li          $v0, 1        # system call code for print_int
-        # move        $a0, $s0      # integer to print // $s1
-        # syscall                   # print it
+            # li          $v0, 4        # system call code for print_str
+            # move        $a0, $t9      # address of string to print
+            # syscall                   # print the string
 
 
-        # End Program
-        li $v0, 10
-        syscall
+            # li          $v0, 5        # system call code for prompt_int
+            # syscall                   # get int
+            # move        $s1, $v0      # save Large Pool in $s1
 
-######### Factorial Subroutine Fall 2016
-#
-# Given n, in register $a0;
-# calculate n!, store and return the result in register $v0
+            # For debugging
+            # li          $v0, 1        # system call code for print_int
+            # move        $a0, $zero    # integer to print // $s1
+            # syscall                   # print it
+            # li          $v0, 1        # system call code for print_int
+            # move        $a0, $t0      # integer to print // $s1
+            # syscall                   # print it
+            # li          $v0, 1        # system call code for print_int
+            # move        $a0, $zero    # integer to print // $s1
+            # syscall                   # print it
 
-factrl: sw $ra, 4($sp)              # save the return address
-        sw $a0, 0($sp)              # save the current value of n
-        addi $sp, $sp, -8           # move stack pointer
-        slti $t0, $a0, 2            # save 1 iteration, n=0 or n=1; n!=1
-        beq $t0, $zero, L1          # not less than 2, calculate n(n-1)!
-        addi $v0, $zero, 1          # n=1; n!=1
-        jr $ra                      # now multiply
+# s0: the running total
+# s1: input string
+# s2:
+# s3: loop counter
 
-L1:     addi $a0, $a0, -1           # n = n-1
-        jal factrl                  # now (n-1)!
-        addi $sp, $sp, 8            # reset the stack pointer
-        lw $a0, 0($sp)              # fetch saved (n-1)
-        lw $ra, 4($sp)              # fetch return address
-        mul $v0, $a0, $v0           # multiply (n)*(n-1)
-        jr $ra                      # return value n!
+# t0 holds the temp decimal value of the character
+# t3-t9 hold the Roman numeral ascii characters
 
-# P Snyder 14 August 2016
-######### End of the subroutine
+            add $s3, $zero, $zero       # initialize loop counter ($s3) to 0
+            # add $s0, $zero, $zero       # initialize running total ($s0) to 0
 
-######### Modified Factorial Subroutine Summer 2018
-#
-# Given n and r, in registers $a0 and $s1;
-# calculate (n)*(n-1)*(n-2)*...(r), store and return the result in register $v0
+            # while count is under 16, move forward through the string starting at the beginning
+prep:       lb $t2, 0($s1)
+            # j print
 
-modfactrl: sw $ra, 4($sp)           # save the return address mod
-        sw $a0, 0($sp)              # save the current value of n
-        addi $sp, $sp, -8           # move stack pointer
-        # slti $t0, $a0, 2          # save 1 iteration, n=0 or n=1; n!=1
-        # beq $t0, $a1, Loop1       # not less than $a1, calculate n(n-1)!
-        slt $t0, $a0, $a1           # save 1 iteration, n=0 or n=1; n!=1
-        beq $t0, $zero, Loop1       # not less than $a0, calculate n(n-1)!
-        addi $v0, $zero, 1          # n=1; n!=1
-        jr $ra                      # now multiply
+            # crosswalk the parsed character through the Roman ascii value
+            lb $t1, 0($t3)              # load I character
+            beq $t2, $t1, I             # if parsed character is I, jump to that function
 
-Loop1:  addi $a0, $a0, -1           # n = n-1
-        jal modfactrl               # now (n-1)!
-        addi $sp, $sp, 8            # reset the stack pointer
-        lw $a0, 0($sp)              # fetch saved (n-1)
-        lw $ra, 4($sp)              # fetch return address
-        mul $v0, $a0, $v0           # multiply (n)*(n-1)
-        jr $ra                      # return value n!
+            lb $t1, 0($t4)              # load V character
+            beq $t2, $t1, V             # if parsed character is V, jump to that function
 
-# P Snyder 14 August 2016, modified by M Shofner 9 July 2018
-######### End of the subroutine
+            lb $t1, 0($t5)              # load X character
+            beq $t2, $t1, X             # if parsed character is X, jump to that function
+
+            lb $t1, 0($t6)              # load L character
+            beq $t2, $t1, L             # if parsed character is L, jump to that function
+
+            lb $t1, 0($t7)              # load C character
+            beq $t2, $t1, C             # if parsed character is C, jump to that function
+
+            lb $t1, 0($t8)              # load D character
+            beq $t2, $t1, D             # if parsed character is D, jump to that function
+
+            lb $t1, 0($t9)              # load M character
+            beq $t2, $t1, M             # if parsed character is M, jump to that function
+
+I:          addi $t0, $zero, 1          # load 1 into $t0
+            j maths
+
+V:          addi $t0, $zero, 5          # load 5 into $t0
+            j maths
+
+X:          addi $t0, $zero, 10         # load 10 into $t0
+            j maths
+
+L:          addi $t0, $zero, 50         # load 50 into $t0
+            j maths
+
+C:          addi $t0, $zero, 100        # load 100 into $t0
+            j maths
+
+D:          addi $t0, $zero, 500        # load 500 into $t0
+            j maths
+
+M:          addi $t0, $zero, 1000       # load 1000 into $t0
+            j maths
+
+invalid:    add $t0, $zero, $zero       # treat invalid input as 0 value
+            j maths
+
+maths:      add $s0, $s0, $t0           # add $t0 to the running total $s0
+
+
+            # for debugging
+print:      li          $v0, 1        # system call code for print_int
+            move        $a0, $s0      # integer to print // $s1
+            syscall                   # print it
+
+            li          $v0, 1        # system call code for print_int
+            move        $a0, $t0      # integer to print // $s1
+            syscall                   # print it
+
+            # End Program
+            li $v0, 10
+            syscall
 
         .data
-str:
-            .asciiz "The odds are 1 in "
-strLrg1:
-            .asciiz "Size of the large pool? \n"
-strLrg2:
-            .asciiz "Picks needed from the large pool? \n"
-strSml1:
-            .asciiz "Size of the small pool? \n"
-strSml2:
-            .asciiz "Picks needed from the small pool? \n"
+str:        .asciiz "Enter the Roman numerals (16 maximum): \n"
+buffer:     .space 16
+romanI:      .asciiz "I"
+romanV:      .asciiz "V"
+romanX:      .asciiz "X"
+romanL:      .asciiz "L"
+romanC:      .asciiz "C"
+romanD:      .asciiz "D"
+romanM:      .asciiz "M"
