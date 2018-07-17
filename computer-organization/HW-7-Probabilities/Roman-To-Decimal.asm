acade2f0897f6b28f1e52c1e80d12ca6a4b5168a
Markham Shofner <markham.shofner@gmail.com>
2018-07-17 15:20:57 -0400

    finish rome to arabia

--- computer-organization/HW-7-Probabilities/Roman-To-Decimal.asm
+++ computer-organization/HW-7-Probabilities/Roman-To-Decimal.asm
@@ -1,22 +1,20 @@
+# Request Roman numeral input string (as upper case ASCII chars) from user
+# Output the related decimal value
 
             .globl main
-main:       # accent Roman numeral input and output its decimal value
+main:       # accept Roman numeral input and output its decimal value
             .text
 
             # grab the Roman string
             li          $v0, 4        # system call code for print_str
-            la          $a0, str      # address of string to print
+            la          $a0, strRom # address of string to print
             syscall                   # print the string
 
             li          $v0, 8        # system call code for read_string
             la          $a0, buffer   # load byte space into arg0
-            li          $a1, 9        # assign byte space allocation in arg1
+            li          $a1, 11       # assign byte space allocation in arg1
             syscall                   # read the string
             la          $s1, buffer   # save the string in $s1
-
-            # li          $v0, 4        # system call code for print_str
-            # move        $a0, $s1      # address of string to print
-            # syscall                   # print the string
 
             la          $t3, romanI   # load I into a temp variable
             la          $t4, romanV   # load V into a temp variable
@@ -28,16 +26,18 @@ main:       # accent Roman numeral input and output its decimal value
 
 # s0: the running total
 # s1: input string
-# s2:
+# s2: previous loop value tracker
 # s3: loop counter
 
-# t0 holds the temp decimal value of the character
-# t3-t9 hold the Roman numeral ascii characters
+# t0: decimal value of the character
+# t1: roman numeral ascii value / loop counter check
+# t2: parsed roman number ascii value
+# t3-t9: Roman numeral ascii characters
 
             add $s3, $zero, $zero       # initialize loop counter ($s3) to 0
-            # add $s0, $zero, $zero       # initialize running total ($s0) to 0
+            add $s2, $zero, $zero       # initialize previous loop value to 0
 
-            # while count is under 8, move forward through the string starting at the beginning
+            # while count is under 10, move forward through the string starting at the beginning
 prep:       lb $t2, 0($s1)
             j triage
 prep1:      lb $t2, 1($s1)
@@ -53,6 +53,10 @@ prep5:      lb $t2, 5($s1)
 prep6:      lb $t2, 6($s1)
             j triage
 prep7:      lb $t2, 7($s1)
+            j triage
+prep8:      lb $t2, 8($s1)
+            j triage
+prep9:      lb $t2, 9($s1)
             j triage
 
 triage:     # crosswalk the parsed character through the Roman ascii value
@@ -104,8 +108,10 @@ invalid:    add $t0, $zero, $zero       # treat invalid input as 0 value
             j maths
 
 maths:      add $s0, $s0, $t0           # add $t0 to the running total $s0
+            slt $s4, $s2, $t0           # is the stored value of the previous loop less than the current loop value char
+            beq $s4, 1, subtract        # if the stored value is less, jump to the subtract method
 
-            # re-loop
+reloop:     add $s2, $t0, $zero         # store $t0 in $s2
             addi $s3, 1                 # add 1 to counter
             addi $t1, $zero, 1          # load int value 1
             beq $s3, $t1, prep1         # if counter is 1, jump to that function
@@ -121,23 +127,38 @@ maths:      add $s0, $s0, $t0           # add $t0 to the running total $s0
             beq $s3, $t1, prep6         # if counter is 6, jump to that function
             addi $t1, $zero, 7          # load int value 7
             beq $s3, $t1, prep7         # if counter is 7, jump to that function
+            addi $t1, $zero, 8          # load int value 8
+            beq $s3, $t1, prep8         # if counter is 8, jump to that function
+            addi $t1, $zero, 9          # load int value 9
+            beq $s3, $t1, prep9         # if counter is 9, jump to that function
+            j print                     # jump to the print function if the loops are finished
 
-            # for debugging
-print:      li          $v0, 1        # system call code for print_int
-            move        $a0, $s0      # integer to print // $s1
+subtract:   sub $s0, $s0, $s2           # subtract to remove addition from first loop
+            sub $s0, $s0, $s2           # subtract again to reduce the value correctly
+            j reloop
+
+
+print:      li          $v0, 4        # system call code for print_str
+            la          $a0, strDec   # address of string to print
+            syscall                   # print the string
+
+            li          $v0, 1        # system call code for print_int
+            move        $a0, $s0      # integer to print
             syscall                   # print it
 
-            # li          $v0, 1        # system call code for print_int
-            # move        $a0, $t0      # integer to print // $s1
-            # syscall                   # print it
+            li          $v0, 4        # system call code for print_str
+            la          $a0, strEnd   # address of string to print
+            syscall                   # print the string
 
             # End Program
             li $v0, 10
             syscall
 
         .data
-str:        .asciiz "Enter the Roman numerals (8 maximum): \n"
-buffer:     .space 9
+strRom:      .asciiz "Enter the Roman numerals (10 maximum): \n"
+strDec:      .asciiz "The equivalent decimal value is: \n"
+strEnd:      .asciiz "\n\n +ProgramEnd+"
+buffer:      .space 11
 romanI:      .asciiz "I"
 romanV:      .asciiz "V"
 romanX:      .asciiz "X"
